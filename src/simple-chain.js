9862bb288b89d7e431aca65d0310d96f4b73a4d4
Kate Lupashtsian <kate.lupashtsian@gmail.com>
2021-03-30 00:03:50 +0300

    feat:add  solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
+ 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let val_str = "( )";
+    if (value == null) {
+      val_str = "( null )";
+    } else if (value != undefined) {
+      val_str = "( " + value + " )";
+    }
+
+    this.chain.push(val_str);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position !== 'number') {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result_str = "";
+    for (let i = 0; i < this.chain.length; i++) {
+      if (i !== 0) {
+        result_str += "~";
+      }
+      result_str += this.chain[i]
+      if (i !== this.chain.length - 1) {
+        result_str += "~";
+      }
+    }
+
+    this.chain = [];
+    return result_str;
   }
 };
 
